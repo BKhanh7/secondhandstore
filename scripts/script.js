17fbb786d5778105b1a87aad3191a06eeace0500
BKhanh7 <nguyenbaokhanhpro2@gmail.com>
2024-11-25 20:44:33 +0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,14 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
-
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll(".Cart");
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            alert("Đây là giỏ hàng, nhưng chưa có sài được!");
+        });
+    });
+});
 document.addEventListener("DOMContentLoaded", () => {
     const menuToggle = document.querySelector(".menu-toggle"); // Nút menu toggle
     const nav = document.querySelector("nav"); // Phần tử nav chứa menu
